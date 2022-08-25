f5b641121e0f8beca11f195de8a17c75dbb243db
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2022-08-25 12:19:15 -0300

    Modificações pequenas na bebidaController (não testado) 2508

--- API_DRINK/Controllers/BebidaController.cs
+++ API_DRINK/Controllers/BebidaController.cs
@@ -4,23 +4,56 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using API_DRINK.Models;
 
 namespace API_DRINK.Controllers
 {
     public class BebidaController : ApiController
-    {
+    {   //Modifiquei daqui para baixo GM 2508
+        List<Bebida> bebidas = new List<Bebida>(new Bebida[] {
+        new Bebida(1, 1, 1, "Água com limão", "Misture o limão com água", "https://gooutside.com.br/wp-content/uploads/sites/3/2020/03/agua-com-limao-realmente-limpa-o-organismo.jpg"),
+        new Bebida(2, 2, 2, "Chá com leite", "Misture leite em pó com chá", "https://www.iguaria.com/wp-content/uploads/2022/02/Iguaria-Receita-de-Leite-com-Cha-Preto.jpg")
+        }); 
+            
+            
+            
         // GET: api/Bebida
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        /*
+
         // GET: api/Bebida/5
         public string Get(int id)
         {
             return "value";
         }
+        */
 
+        //GET: api/Bebida/getAll
+
+        [HttpGet]
+        [ActionName("getAll")]
+        public IEnumerable GetAllBebidas()
+        {
+            try
+            {
+                DBConnection db = new DBConnection();
+                var registro = db.MostraTodos();
+                db.Fechar();
+                return registro;
+            }
+            catch(Exception e)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                throw new HttpResponseException(resp);
+            }
+        }
+
+        //Modifiquei acima GM 2508
+        
         // POST: api/Bebida
         public void Post([FromBody]string value)
         {
