b6e77377b4579f64e6a63885b3165ee9bdfd0582
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2022-09-13 21:28:27 -0300

    Adição de uma model (IProductRepository)
    Incompleta (13/09)

---
+++ API_DRINK/Models/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_DRINK.Models
+{
+    public class IProductRepository
+    {
+        IEnumerable<Bebida> GetAll();
+        Bebida Get(int id);
+        Bebida Add(Bebida registro);
+        void Remove(int id);
+        bool Update(Bebida registro);
+    }
+}
